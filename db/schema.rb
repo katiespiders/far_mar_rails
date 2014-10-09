4f39450985a7d4824ba92b2464b841789ca892bb
Katie <kab761@gmail.com>
2014-10-09 14:30:03 -0700

    changed price to decimal in activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009205732) do
+ActiveRecord::Schema.define(version: 20141009212853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20141009205732) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
-    t.integer  "price"
+    t.decimal  "price"
   end
 
   create_table "vendors", force: true do |t|
